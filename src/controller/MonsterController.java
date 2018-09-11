6603d3ad8877ec700ed1df344eef4a253735bd20
Hurricane36 <43070976+Hurricane36@users.noreply.github.com>
2018-09-11 09:32:46 -0600

    customized the monster

--- src/controller/MonsterController.java
+++ src/controller/MonsterController.java
@@ -1,6 +1,25 @@
 package controller;
 
+import model.MarshmellowMonster;
+
 public class MonsterController
 {
+	//Data member Section
+	private MarshmellowMonster myMonster;
+	
+	//Constructor section
+	public MonsterController()
+	{
+		//Job 1: initialize data members!
+		myMonster = new MarshmellowMonster("TheFailure",3.75,3,false,3);
+	}
+	
+	//method section
+	public void start()
+	{
+		System.out.println(myMonster);
+		
+		
+	}
 
 }
--- src/model/MarshmellowMonster.java
+++ src/model/MarshmellowMonster.java
@@ -2,5 +2,43 @@ package model;
 
 public class MarshmellowMonster
 {
+	//------Data Member Section-----------
+	//They are ALWAYS private
+	private String name;
+	private double legCount;
+	private int eyeCount;
+	private boolean hasNoses;
+	private int armCount;
+	
+	
+	public MarshmellowMonster()
+	{
+		//Default values are 0 or null
+		
+	}
+	
+	public MarshmellowMonster(String name)
+	{
+		this.name = name;
+	}
+	
+	
+	public MarshmellowMonster(String name, double legCount, int eyeCount, boolean hasNoses, int armCount)
+	{
+		this.name = name;
+		this.legCount = legCount;
+		this.eyeCount = eyeCount;
+		this.hasNoses = hasNoses;
+		this.armCount = armCount;
+		
+			
+	}
 
+	public String toString()
+	{
+		String description = "My monsters name is"+ name + "She has" + legCount + "legs" + eyeCount + "eyes"+ hasNoses + "Nose" + armCount + "arms";
+		
+		return description;
+	}
+	
 }
